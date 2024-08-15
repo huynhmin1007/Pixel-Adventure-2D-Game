19c8185c513a7b957e730f4cb09f4aa4cc8afd7d
Chuc-ngan <21130451@st.hcmuaf.edu.vn>
2024-08-15 19:11:08 +0700

    hero

---
+++ Assets/Scripts/Characters/HeroTracker/HeroTrackerBattleState.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Scripts.Base;
+using Assets.Scripts.Characters.Enemy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Characters.HeroTracker
+{
+    public class HeroTrackerBattleState : EnemyBattleState
+    {
+
+        private HeroTrackerCharacter character;
+        public HeroTrackerBattleState(EnemyCharacter characterBase, StateMachine stateMachine, Enum animBoolName) : base(characterBase, stateMachine, animBoolName)
+        {
+            this.character = (HeroTrackerCharacter)characterBase;
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/Characters/HeroTracker/HeroTrackerCharacter.cs
@@ -0,0 +1,40 @@
+﻿using Assets.Scripts.Base.State;
+using Assets.Scripts.Characters.Enemy;
+using Assets.Scripts.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Characters.HeroTracker
+{
+    public class HeroTrackerCharacter : EnemyCharacter
+    {
+        public override bool CanBattle()
+        {
+            return CanPrimaryAttack();
+        }
+
+        public override void HandleBattle()
+        {
+            
+        }
+
+        protected override void InitializeStates()
+        {
+            states.Add(EState.Idle, new EnemyIdleState(this, stateMachine, EState.Idle));
+            states.Add(EState.Move, new EnemyMoveState(this, stateMachine, EState.Move));
+            states.Add(EState.Fall, new FallState(this, stateMachine, EState.Air));
+            states.Add(EState.Battle, new HeroTrackerBattleState(this, stateMachine, EState.Move));
+            states.Add(EState.Jump, new JumpState(this, stateMachine, EState.Air));
+        }
+
+        protected override void StateController()
+        {
+            
+
+        }
+    }
+}
