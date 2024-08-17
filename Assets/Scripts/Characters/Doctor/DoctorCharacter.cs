01531d82856e989edbd5dfccb7d1bf39a84eedf2
Chuc-ngan <21130451@st.hcmuaf.edu.vn>
2024-08-17 14:40:55 +0700

    ADD BACKGROUND

--- Assets/Scripts/Characters/Doctor/DoctorCharacter.cs
+++ Assets/Scripts/Characters/Doctor/DoctorCharacter.cs
@@ -12,13 +12,59 @@ namespace Assets.Scripts.Characters.Doctor
     {
         public override bool CanBattle()
         {
-            return CanPrimaryAttack();
+            /**
+             * Kiểm tra có phát hiện Player không (tia thẳng màu vàng)
+             */
+            bool isPlayerDetected = IsPlayerDetected();
+
+            if (isPlayerDetected)
+            {
+                /**
+                 * Nếu player trong phạm vi tấn công của enemy
+                 */
+                if (IsPlayerInAttackRange())
+                {
+                    /**
+                     * Nếu có thể tấn công bình thường
+                     * (Check dựa vào cooldown tránh enemy tấn công liên tục không nghỉ)
+                     */
+                    if (CanPrimaryAttack())
+                    {
+                        /**
+                         * Chuyển sang trạng thái đánh thường
+                         */
+                        stateMachine.ChangeState(states[EState.Attack]);
+                    }
+                    /**
+                     * Nếu ko thể tấn công bình thường và player đang trong phạm vi tấn công của enemy
+                     * Enemy dùng Animation Idle, đứng thở chờ tới khi được đánh tiếp
+                     */
+                    else
+                        stateMachine.ChangeAnimation(EState.Idle);
+                    /**
+                     * Trả về true (vẫn có thể tiếp tục battle, theo đuổi player)
+                     * (Check lại EnemyBattleState coi logic của CanBattle() khi trả về true - false)
+                     */
+                    return true;
+                }
+                /**
+                 * Nếu player ko trong phạm vi tấn công của enemy
+                 * Di chuyển đến chỗ player
+                 */
+                else
+                {
+                    stateMachine.ChangeAnimation(EState.Move);
+                    SetVelocity(Direction.XValue() * MoveSpeed, YVelocity);
+                }
+            }
+
+            /**
+             * Trả về giá trị có phát hiện player không để quyết định nên theo đuổi tiếp hay không
+             */
+            return isPlayerDetected;
         }
 
-        public override void HandleBattle()
-        {
-           
-        }
+       
 
         protected override void InitializeStates()
         {
